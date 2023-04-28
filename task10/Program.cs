88cc19e92da48fd9ac272e14eab65650224313b5
ArtemUb <savenkoam@gmail.com>
2023-04-28 19:51:38 +0900

    done task 15

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа. Через строку решать нельзя.
+Random rnd = new Random();
+int num = rnd.Next(100, 1000);
+Console.WriteLine($"Random number = {num}");
+
+num = (num / 10) % 10;
+
+Console.WriteLine(num);
---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 6 | num == 7)
+{
+    Console.WriteLine("Yes");
+}
+else if (num < 1 | num > 7)
+{
+    Console.WriteLine("Enter number from 1 to 7");
+}
+else
+{
+    Console.WriteLine("No");
+}
